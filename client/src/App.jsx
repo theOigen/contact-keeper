a8eb34ed48647ccfc92719f82ca366c9923ee7ae
theoigen <oigenZ@icloud.com>
2019-07-14 21:02:49 +0300

    added base contact context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
-
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+
+import ContactState from './context/contact/ContactState';
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Container>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-          </Switch>
-        </Container>
-      </>
-    </Router>
+    <ContactState>
+      <Router>
+        <>
+          <Navbar />
+          <Container>
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+            </Switch>
+          </Container>
+        </>
+      </Router>
+    </ContactState>
   );
 };
 
