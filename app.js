53f1f28a37319411949ae5ce1f631acf7c650e4b
theoigen <oigenZ@icloud.com>
2019-07-20 17:43:30 +0300

    Prepared for deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const { PORT } = require('./config/server');
+const path = require('path');
 
 const app = express();
 
@@ -13,5 +14,14 @@ app.use(express.json({ extended: false }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
